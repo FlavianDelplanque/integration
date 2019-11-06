fba8e70abfb8ede144c6777ebde7bbbe3f6a9167
FlavianDelplanque <delplanqueflavian@gmail.com>
2019-11-06 17:02:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web design</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style-desktop.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="logomaquette">
+				<img src="images/petit-logo.png">
+				<h2>Maquette</h2>
+			</div>
+			<div class="listenav">
+				<ul>
+					<li>Accueil</li>
+					<li>A propos</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	<div class="accueil page center">
+		<img src="images/main-logo.png">
+		<div id="gradiant"></div>
+		<div id="presentation">
+			<p>Lorem <strong>ipsum</strong> dolor sit amet, <strong>consectetur</strong></p>
+			<h1>Maquette web design</h1>
+			<h1>Développeur web</h1>
+		</div>
+	</div>
+	<div class="competence page center">
+		<section>
+			<div class="section">
+				<div class="design center">
+					<h3>Ui Ux Design</h3>
+					<p>exercitation ullamco laboris nisi</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt</p>
+				</div>
+				<div class="info center">
+					<table>
+						<thead>
+							<tr>
+								<th colspan="2">Lorem</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td id="info1">Info 1</td>
+								<td id="info2">Info 2</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="section" id="section">
+				<div class="savoir center">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+					<button>En savoir plus</button>
+				</div>
+				<div class="design center">
+					<h3>Responsive Design</h3>
+					<p>exercitation ullamco laboris nisi</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt</p>
+				</div>
+			</div>
+		</section>
+	</div>
+	<div class="services page">
+		<div class="descservice">
+			<h2>Services</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+		</div>
+		<section>
+			<div class="service">
+				<img src="images/main-logo.png">
+				<h3>Interface design</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			</div>
+			<div class="service">
+				<img src="images/main-logo.png">
+				<h3>Theming</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			</div>
+			<div class="service">
+				<img src="images/main-logo.png">
+				<h3>Referencement</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			</div>
+			<div class="service">
+				<img src="images/main-logo.png">
+				<h3>Mobiles app</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			</div>
+			<div class="service">
+				<img src="images/main-logo.png">
+				<h3>Responsive design</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			</div>
+			<div class="service">
+				<img src="images/main-logo.png">
+				<h3>Free updates</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+			</div>
+		</section>	
+	</div>
+	<footer>
+		
+	</footer>
+</body>
+</html>
